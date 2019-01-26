edb065d43285fa93e2f2c87ccf4075e5e83870cc
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-26 17:13:19 +0100

    Add create endpoint

--- server/server.js
+++ server/server.js
@@ -1,29 +1,32 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+const { mongoose } = require("./db/mongoose");
+const { Todo } = require("./models/todo");
+const { User } = require("./models/user");
 
-const Todo = mongoose.model("Todo", {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
-  }
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+  console.log(req.body);
+  const todo = new Todo({
+    text: req.body.text
+  });
+
+  todo
+    .save()
+    .then(doc => {
+      res.status(201).send(doc);
+    })
+    .catch(e => {
+      console.log(e);
+
+      res.status(400).send(e);
+    });
 });
 
-const newTodo = new Todo({
-  text: "Cook dinner cat",
-  completed: true,
-  completedAt: 123
+app.listen(3000, () => {
+  console.log("Started on port 3000");
 });
-
-newTodo.save().then(
-  doc => {
-    console.log("Saved todo", doc);
-  },
-  e => console.log("Unable to save todo")
-);

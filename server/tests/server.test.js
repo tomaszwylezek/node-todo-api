bf5e30392b94e3299da97466184c3ac30f631961
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-27 16:45:16 +0100

    Setup mock data for users && refactor

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,28 +5,10 @@ const request = require("supertest");
 const { app } = require("../server");
 const { Todo } = require("../models/todo");
 
-const todos = [
-  {
-    _id: new ObjectID(),
-    text: "First test todo"
-  },
-  {
-    _id: new ObjectID(),
-    text: "Second test todo",
-    completed: true,
-    completedAt: 333
-  }
-];
+const { populateTodos, todos, populateUsers, users } = require("./seed/seed");
 
-beforeEach(done => {
-  Todo.remove({})
-    .then(() => {
-      return Todo.insertMany(todos);
-    })
-    .then(() => {
-      done();
-    });
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe("POST /todos", () => {
   it("should create a new todo", done => {
